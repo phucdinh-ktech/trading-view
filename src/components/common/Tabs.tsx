0da8ee0ab3701533ad92da041c4983b406209023
dangvietquang59 <dvquang5902@gmail.com>
2024-11-19 10:03:24 +0700

    feat: create ui blog

--- src/components/common/Tabs.tsx
+++ src/components/common/Tabs.tsx
@@ -1,35 +1,17 @@
 import { Tabs, TabsProps } from "antd";
 
-import WrapperMarket from "@/components/pages/home/WrapperMarket";
+interface TabsComponentProps extends TabsProps {
+  extraProp?: string;
+}
 
-const TabsComponent = () => {
-  const items: TabsProps["items"] = [
-    {
-      key: "1",
-      label: "Indices",
-      children: <WrapperMarket />,
-    },
-    {
-      key: "2",
-      label: "Stocks",
-      children: <WrapperMarket />,
-    },
-    {
-      key: "3",
-      label: "ETFs",
-      children: <WrapperMarket />,
-    },
-  ];
-  const onChange = (key: string) => {
-    console.log(key);
-  };
+const TabsComponent = ({ items, onChange, ...rest }: TabsComponentProps) => {
   return (
     <Tabs
-      defaultActiveKey="1"
       items={items}
       onChange={onChange}
       rootClassName="tabs-custom"
       className="p-[4px]"
+      {...rest}
     />
   );
 };
